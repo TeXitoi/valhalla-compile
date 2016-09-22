58d628bb1f42463b10537785d9a046d22f0b5072
Guillaume Pinot <texitoi@texitoi.eu>
2016-09-22 17:09:31 +0200

    add possibility to skip autogen, configure and update to master

--- compile.sh
+++ compile.sh
@@ -5,6 +5,43 @@ local="$work_dir/local"
 export PKG_CONFIG_PATH=":$local/lib/pkgconfig"
 export CPPFLAGS="-DBOOST_SPIRIT_THREADSAFE -DBOOST_NO_CXX11_SCOPED_ENUMS"
 export CXXFLAGS="-I$local/include"
+export INSTALL="/usr/bin/install -C"
+
+usage () {
+    printf 'Usage: %s [OPTION]...\n' "$0"
+    printf 'Checkout, compile and install Valhalla.\n\n'
+    printf '  -h  print this message and exit\n'
+    printf '  -a  skip autogen\n'
+    printf '  -c  skip configure\n'
+    printf '  -m  update to master\n'
+}
+skip_autogen=NO
+skip_configure=NO
+goto_master=NO
+while getopts 'hacm' option; do
+    case "$option" in
+        h)
+            usage
+            exit 0
+            ;;
+        a)
+            echo '+ Will skip autogen'
+            skip_autogen=YES
+            ;;
+        c)
+            echo '+ Will skip configure'
+            skip_configure=YES
+            ;;
+        m)
+            echo '+ Will update to master'
+            goto_master=YES
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
 
 cd "$work_dir"
 mkdir -p "$local"
@@ -14,6 +51,13 @@ run () {
     $* || exit 1
 }
 
+is_dirty () {
+    git diff-index --quiet HEAD || return 0 # uncommited files in index
+    u="$(git ls-files --exclude-standard --others)" || return 0 # ls-files errored
+    [ -z "$u" ] || return 0 # there is untracked files
+    return 1
+}
+
 clone_compile_install () {
     git_base_url="$1"
     project="$2"
@@ -21,8 +65,20 @@ clone_compile_install () {
         run git clone --recursive "$git_base_url${project}.git"
     fi
     run cd "$project"
-    run ./autogen.sh
-    run ./configure --prefix="$local"
+    if [ "$goto_master" = YES ]; then
+        if is_dirty; then
+            printf '+ %s is dirty, stopping.\n' "$project"
+            exit 1
+        fi
+        run git checkout master
+        run git pull --ff-only
+    fi
+    if [ "$skip_autogen" != YES ]; then
+        run ./autogen.sh
+    fi
+    if [ "$skip_configure" != YES ]; then
+        run ./configure --prefix="$local"
+    fi
     run make -j`nproc`
     run make install
     cd ..
